be796930fa387c5571fc5e1d1bb50e72f4993997
rahaf <rahaf.khalid6666@gmail.com>
2026-06-20 16:26:10 +0300

    Add solution: Exercise 1.5: Compute Expressions

---
+++ Chapter_1/exercise_5.java
@@ -0,0 +1,16 @@
+/*
+ * Exercise 1.5: Compute Expressions
+ *
+ * Write a program that displays the result of the following expression: (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5). The class name must be Exercise01_05.
+
+ * Expected Output:
+ *   0.8392857142857143
+ */
+public class Exercise01_05 {
+    public static void main(String[] args) {
+        // Calculate and print result here
+        
+        double result = (9.5 * 4.5 - 2.5 * 3) / (45.5- 3.5);
+        system.out.println("Result:"+result);
+    }
+}
